ceaeadf8bf376dbd4354ebe94d78a8bb0953754e
Dz0nZ1 <dzonzi777@gmail.com>
2023-11-22 19:00:17 +0100

    Created Membership Service

---
+++ src/main/java/com/example/posganize/services/membership/MembershipService.java
@@ -0,0 +1,23 @@
+package com.example.posganize.services.membership;
+
+import com.example.posganize.models.CreateMembershipModel;
+import com.example.posganize.models.MembershipModel;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MembershipService {
+
+    List<MembershipModel> getAllMemberships();
+
+    MembershipModel getMembership(Long membershipId);
+
+    MembershipModel createMembership (CreateMembershipModel membershipModel);
+
+    MembershipModel updateMembership (MembershipModel membershipModel, Long membershipId);
+
+    void deleteMembership(Long membershipId);
+
+    Map<String, Boolean> isActiveMembershipByUserId(Long userId);
+
+}
