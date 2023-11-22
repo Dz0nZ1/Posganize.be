8770007044bb586b3728b0671e04dfe9580a050d
Dz0nZ1 <dzonzi777@gmail.com>
2023-11-22 18:59:34 +0100

    Created Membership Entity

---
+++ src/main/java/com/example/posganize/entities/Membership.java
@@ -0,0 +1,47 @@
+package com.example.posganize.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "membership")
+public class Membership {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long membership_id;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime expireDate;
+
+    @JsonBackReference
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private Users user;
+
+    @JsonManagedReference
+    @ToString.Exclude
+    @OneToOne(mappedBy = "membership", cascade = CascadeType.ALL)
+    private Payments payments;
+
+    @ManyToMany
+    @JoinTable(
+            name = "membership_training",
+            joinColumns = @JoinColumn(name = "membership_id"),
+            inverseJoinColumns = @JoinColumn(name = "training_id"))
+    private List<Training> trainings;
+
+    private Double price;
+    private Boolean active;
+
+}
