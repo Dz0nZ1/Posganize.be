0b05a62b23804f8ca904bae016417a0a83a63461
Dz0nZ1 <dzonzi777@gmail.com>
2023-11-26 04:33:23 +0100

    Created UserStatusException

---
+++ src/main/java/com/example/posganize/exceptions/UserStatusException.java
@@ -0,0 +1,8 @@
+package com.example.posganize.exceptions;
+
+public class UserStatusException extends RuntimeException{
+
+    public UserStatusException(String message) {
+        super(message);
+    }
+}
