7982a53390ded47dfe6bc20161a2c428dde88616
Dz0nZ1 <dzonzi777@gmail.com>
2023-11-30 18:37:23 +0100

    Added pageable object to the findAllMembershipByUserId method

--- src/main/java/com/example/posganize/repository/MembershipRepository.java
+++ src/main/java/com/example/posganize/repository/MembershipRepository.java
@@ -2,12 +2,12 @@ package com.example.posganize.repository;
 
 import com.example.posganize.entities.Membership;
 import com.example.posganize.entities.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership,Long> {
@@ -15,7 +15,6 @@ public interface MembershipRepository extends JpaRepository<Membership,Long> {
 
     @Query(value = "SELECT *\n" +
             "FROM membership\n" +
-            "WHERE user_id = :userId\n" +
-            "ORDER BY expire_date DESC", nativeQuery = true)
-    List<Membership> findAllMembershipByUserId(@Param("userId") Long userId);
+            "WHERE user_id = :userId\n", nativeQuery = true)
+    Page<Membership> findAllMembershipByUserId(@Param("userId") Long userId, Pageable pageable);
 }
