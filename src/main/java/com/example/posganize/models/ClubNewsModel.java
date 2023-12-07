e9e92eafaf8dd6b895fbcfde8c0cd21465a64b45
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-07 21:41:42 +0100

    Added validation annotation to ClubNewsModel

--- src/main/java/com/example/posganize/models/ClubNewsModel.java
+++ src/main/java/com/example/posganize/models/ClubNewsModel.java
@@ -1,5 +1,6 @@
 package com.example.posganize.models;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,9 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ClubNewsModel {
 
+    @NotBlank(message = "Club news name is mandatory")
     private String name;
+    @NotBlank(message = "Description is mandatory")
     private String description;
 
 }
