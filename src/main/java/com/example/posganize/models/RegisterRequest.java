3f8f4080299a4b66aefc05d2885874c657e99eda
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-07 21:40:57 +0100

    Added validation annotation to RegisterRequest

--- src/main/java/com/example/posganize/models/RegisterRequest.java
+++ src/main/java/com/example/posganize/models/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.example.posganize.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +13,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "First name is mandatory")
     private String firstName;
+    @NotBlank(message = "Last name is mandatory")
     private String lastName;
+    @NotBlank(message = "Phone number is mandatory")
     private String phoneNumber;
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email address must be in valid format")
     private String email;
+    @NotBlank(message = "Password is mandatory")
+    @Min(value = 8, message = "Password must have at least 8 characters")
     private String password;
 }
