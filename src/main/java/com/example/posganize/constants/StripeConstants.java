fb15b4e465c8b3f9bbbe821b5964f51881faf8c8
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-18 18:55:03 +0100

    Created StripeConstants

---
+++ src/main/java/com/example/posganize/constants/StripeConstants.java
@@ -0,0 +1,7 @@
+package com.example.posganize.constants;
+
+public class StripeConstants {
+
+    public static String STRIPE_SECRET_KEY = "sk_test_51IFmUZLdh65NywlmgDVk8VXxz0yjP7Tna6PSJuwthDsSpiLHWPmeoYSGgaE5cWA8BiIsQlq7qMP70PZQgZ7wilbl0041V6FD6g";
+
+}
