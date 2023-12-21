7e3a974b7e16d736350cb2138e2210b2bbc3c87a
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-21 09:48:54 +0100

    Created CreateTrainingModel

---
+++ src/main/java/com/example/posganize/models/training/CreateTrainingModel.java
@@ -0,0 +1,28 @@
+package com.example.posganize.models.training;
+
+import com.example.posganize.models.ScheduleModel;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateTrainingModel {
+
+    @NotBlank(message = "Training name is mandatory")
+    private String name;
+    @NotBlank(message = "Description name is mandatory")
+    private String description;
+    @NotBlank(message = "Image name is mandatory")
+    private String image;
+    @NotNull(message = "Price is mandatory")
+    private Double price;
+    private List<ScheduleModel> schedule;
+}
