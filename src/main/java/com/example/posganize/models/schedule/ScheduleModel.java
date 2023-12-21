9c4070bb3adfcded0769fbb0196b2c3b3329c0b5
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-21 18:53:16 +0100

    Created ScheduleModel

---
+++ src/main/java/com/example/posganize/models/schedule/ScheduleModel.java
@@ -0,0 +1,18 @@
+package com.example.posganize.models.schedule;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ScheduleModel {
+    private Long id;
+    private String scheduleName;
+    private String scheduleDay;
+    private String scheduleTime;
+}
