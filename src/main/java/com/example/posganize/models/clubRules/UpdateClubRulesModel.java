b3c5a2a8cf6cc7f38de65e2912fd5df79f3c69d9
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-26 07:08:58 +0100

    Created UpdateClubRulesModel

---
+++ src/main/java/com/example/posganize/models/clubRules/UpdateClubRulesModel.java
@@ -0,0 +1,20 @@
+package com.example.posganize.models.clubRules;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateClubRulesModel {
+    @NotBlank(message = "Image is mandatory")
+    private String image;
+    @NotBlank(message = "Club news name is mandatory")
+    private String name;
+    @NotBlank(message = "Description is mandatory")
+    private String description;
+}
