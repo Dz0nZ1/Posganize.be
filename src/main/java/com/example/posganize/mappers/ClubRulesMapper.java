df9280b94f56f3465865bef9ca7b9c3801069904
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-26 07:11:36 +0100

    Added two new methods for mapping the models

--- src/main/java/com/example/posganize/mappers/ClubRulesMapper.java
+++ src/main/java/com/example/posganize/mappers/ClubRulesMapper.java
@@ -1,7 +1,9 @@
 package com.example.posganize.mappers;
 
 import com.example.posganize.entities.ClubRules;
-import com.example.posganize.models.ClubRulesModel;
+import com.example.posganize.models.clubRules.ClubRulesModel;
+import com.example.posganize.models.clubRules.CreateClubRulesModel;
+import com.example.posganize.models.clubRules.UpdateClubRulesModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,8 @@ public class ClubRulesMapper {
 
     public static ClubRulesModel mapClubRulesToClubRulesModel(ClubRules clubRules) {
         return ClubRulesModel.builder()
+                .id(clubRules.getRules_id())
+                .image(clubRules.getImage())
                 .name(clubRules.getName())
                 .description(clubRules.getDescription())
                 .build();
@@ -18,10 +22,29 @@ public class ClubRulesMapper {
 
     public static ClubRules mapClubRulesModelToClubRules(ClubRulesModel clubRulesModel) {
         return ClubRules.builder()
+                .rules_id(clubRulesModel.getId())
+                .image(clubRulesModel.getImage())
                 .name(clubRulesModel.getName())
                 .description(clubRulesModel.getDescription())
                 .build();
     }
+
+    public static ClubRules mapCreateClubRulesModelToClubRules(CreateClubRulesModel clubRulesModel) {
+        return ClubRules.builder()
+                .image(clubRulesModel.getImage())
+                .name(clubRulesModel.getName())
+                .description(clubRulesModel.getDescription())
+                .build();
+    }
+
+    public static ClubRules mapUpdateClubRulesModelToClubRules(UpdateClubRulesModel clubRulesModel) {
+        return ClubRules.builder()
+                .image(clubRulesModel.getImage())
+                .name(clubRulesModel.getName())
+                .description(clubRulesModel.getDescription())
+                .build();
+    }
+
 
 
     public static List<ClubRulesModel> mapClubRulesListToClubRulesModelList(List<ClubRules> clubRules) {
