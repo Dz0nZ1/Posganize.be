20401e191138c4631a91b352c820ee985fe57786
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-27 12:53:22 +0100

    Updated RegisterRequest with image property

--- src/main/java/com/example/posganize/models/auth/RegisterRequest.java
+++ src/main/java/com/example/posganize/models/auth/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.example.posganize.models.auth;
 
+import jakarta.persistence.Lob;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -25,4 +26,6 @@ public class RegisterRequest {
     @NotBlank(message = "Password is mandatory")
     @Min(value = 8, message = "Password must have at least 8 characters")
     private String password;
+    @Lob
+    private byte[] image;
 }
