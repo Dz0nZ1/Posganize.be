6dc844ae30edf98f465cbcc7cfa514e8363cddac
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-28 15:54:08 +0100

    Updated StripeController with stripe web hook

--- src/main/java/com/example/posganize/controllers/StripeController.java
+++ src/main/java/com/example/posganize/controllers/StripeController.java
@@ -2,27 +2,47 @@ package com.example.posganize.controllers;
 
 import com.example.posganize.constants.StripeConstants;
 import com.example.posganize.enums.CurrencyEnum;
+import com.example.posganize.models.membership.CreateMembershipModel;
 import com.example.posganize.models.stripe.*;
 import com.example.posganize.models.training.TrainingModel;
+import com.example.posganize.services.membership.MembershipService;
+import com.example.posganize.services.training.TrainingService;
+import com.example.posganize.utils.CustomerUtil;
 import com.stripe.Stripe;
+import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Customer;
+import com.stripe.model.*;
 import com.stripe.model.checkout.Session;
+import com.stripe.net.Webhook;
 import com.stripe.param.checkout.SessionCreateParams;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1/stripe")
+@Slf4j
 public class StripeController {
 
     String STRIPE_API_KEY = StripeConstants.STRIPE_SECRET_KEY;
 
-//    @PostMapping("/checkout/hosted")
-//    String hostedCheckout(@RequestBody RequestDTO requestDTO) throws StripeException {
-//        return "Hello World!";
-//    }
+    private final TrainingService trainingService;
+    private final MembershipService membershipService;
+    private final String endpointSecret = "whsec_81f3832b9fe0f8a2bdc39677e4f9703e8ec2a16ed00a325bceee691d6f427f78";
+    private static final Logger LOGGER = LoggerFactory.getLogger(StripeController.class);
+
+    public StripeController(TrainingService trainingService, MembershipService membershipService) {
+        this.trainingService = trainingService;
+        this.membershipService = membershipService;
+    }
+
 
     @PostMapping("/checkout")
     String hostedCheckout(@RequestBody RequestDTO requestDTO) throws StripeException {
@@ -32,6 +52,11 @@ public class StripeController {
 
         // Start by finding an existing customer record from Stripe or creating a new one if needed
         Customer customer = CustomerUtil.findOrCreateCustomer(requestDTO.getCustomerEmail(), requestDTO.getCustomerName());
+
+
+        List<Long> trainingModelIds = requestDTO.getItems().stream().map(TrainingModel::getId)
+                .toList();
+
 
         // Next, create a checkout session by adding the details of the checkout
         SessionCreateParams.Builder paramsBuilder =
@@ -54,9 +79,15 @@ public class StripeController {
                                                             .build()
                                             )
                                             .setCurrency(CurrencyEnum.USD.toString())
-                                            .setUnitAmountDecimal(BigDecimal.valueOf(trainingModel.getPrice()*100))
+                                            .setUnitAmountDecimal(BigDecimal.valueOf(trainingModel.getPrice() * 100))
                                             .build())
                             .build());
+            paramsBuilder.setPaymentIntentData(
+                    SessionCreateParams.PaymentIntentData.builder()
+                            .putMetadata("trainingModelIds", trainingModelIds.toString())
+                            .putMetadata("email", requestDTO.getCustomerEmail())
+                            .build()
+            );
         }
 
         Session session = Session.create(paramsBuilder.build());
@@ -64,33 +95,84 @@ public class StripeController {
         return session.getUrl();
     }
 
+    @PostMapping("/webhook")
+    public ResponseEntity<String> handleStripeWebhook(@RequestBody String payload,
+                                                      @RequestHeader("Stripe-Signature") String sigHeader) {
+        try {
+            Event event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
+
+            // Handle the event
+            switch (event.getType()) {
+                case "payment_intent.succeeded":
+
+                    PaymentIntent paymentIntent = null;
+
+                    // Get the StripeObject from the event
+                    var stripeObjectOptional = event.getDataObjectDeserializer().getObject();
+
+                    // Check if the StripeObject is present and if it's an instance of PaymentIntent
+                    if (stripeObjectOptional.isPresent() && stripeObjectOptional.get() instanceof PaymentIntent) {
+                        paymentIntent = (PaymentIntent) stripeObjectOptional.get();
+                    } else {
+                        // Handle the case where the object is not a PaymentIntent
+                        // You might log an error or perform other error-handling actions
+                        System.out.println("Unexpected object type or missing PaymentIntent");
+                    }
+
+                    if (paymentIntent != null) {
+                        Map<String, String> metadata = paymentIntent.getMetadata();
+
+                        String trainingModelIdsString = metadata.get("trainingModelIds");
+
+                        String email = (String) metadata.get("email");
+                        List<Long> listData = parseStringToList(trainingModelIdsString);
+
+                        Set<TrainingModel> trainingModels = new HashSet<>();
+                        for (Long trainingId : listData) {
+                            var training = trainingService.getTrainingById(trainingId);
+                            trainingModels.add(training);
+                        }
+
+                        CreateMembershipModel createMembershipModel = CreateMembershipModel
+                                .builder()
+                                .trainings(trainingModels)
+                                .startDate(LocalDateTime.now())
+                                .expireDate(LocalDateTime.now().plusDays(30))
+                                .build();
+
+                        membershipService.createMembershipByUserEmail(createMembershipModel, email);
+                        System.out.println("Membership successfully purchased");
 
 
+                    }
 
-//    private final StripeService stripeService;
-//
-//    public StripeController(StripeService stripeService) {
-//        this.stripeService = stripeService;
-//    }
-//
-//    @PostMapping("/card/token")
-//    @ResponseBody
-//    public StripeTokenModel createCardToken(@RequestBody StripeTokenModel model) {
-//        return stripeService.createCardToken(model);
-//    }
-//
-//    @PostMapping("/charge")
-//    @ResponseBody
-//    public StripeChargeModel charge(@RequestBody StripeChargeModel model) {
-//        return stripeService.charge(model);
-//    }
-//
-//
-//    @PostMapping("/customer/subscription")
-//    @ResponseBody
-//    public StripeSubscriptionResponse subscription(@RequestBody StripeSubscriptionModel model) {
-//        return stripeService.createSubscription(model);
-//    }
+                    break;
+                case "checkout.session.completed":
+                    System.out.println("success");
+                default:
+                    System.out.println("Unhandled event type: " + event.getType());
+                    break;
+            }
 
+            return ResponseEntity.status(HttpStatus.OK).body("Received and processed the event.");
+        } catch (SignatureVerificationException e) {
+            // Invalid signature
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid signature.");
+        } catch (Exception e) {
+            // Handle other exceptions or log the error
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing the event.");
+        }
+    }
 
+    public static List<Long> parseStringToList(String input) {
+        input = input.substring(1, input.length() - 1); // Remove brackets [ and ]
+        String[] elements = input.split(", "); // Split by comma and space
+
+        List<Long> result = new ArrayList<>();
+        for (String element : elements) {
+            result.add(Long.parseLong(element));
+        }
+
+        return result;
+    }
 }
