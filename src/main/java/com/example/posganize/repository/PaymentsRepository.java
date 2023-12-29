4cad326828efc01d78e25e9f9859be9535109684
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-29 12:37:39 +0100

    Deleted everything form deprecated payment entity

--- src/main/java/com/example/posganize/repository/PaymentsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.posganize.repository;
-
-import com.example.posganize.entities.Payments;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentsRepository extends JpaRepository<Payments, Long> {
-}
